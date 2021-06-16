38b6c2d5c92bf9c6353eaea27bf0531321b70de3
gbstrom <gbstrom@gmail.com>
2021-06-16 15:33:58 -0400

    added pokemonList array with three pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
-alert('Hello world');
-let favoriteFood = 'Lobster Thermidor';
-document.write(favoriteFood);
+let Abra = {
+  name: 'Abra',
+  height: 8,
+  type: ['psychic']
+}
+
+let Doduo = {
+  name: 'Doduo',
+  height: 10,
+  type: ['normal', 'flying']
+}
+
+let Psyduck = {
+  name: 'Psyduck',
+  height: 5,
+  type: ['water']
+}
+let pokemonList = [Abra, Doduo, Psyduck];
