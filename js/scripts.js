a3e8d3ae3519fa06f64cec677f81c3bb2239a9aa
gbstrom <gbstrom@gmail.com>
2021-06-25 13:07:15 -0400

    introduced AJAX connection to Pokemon API and made appropriate adjustments to the code for presenting Pokemon names and data asynchronously

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,12 @@
 // this is an IIFE that wraps up much of my code to minimize side-effects.
 
 let pokemonRepository = (function () {
-  let pokemonList = [
+  let pokemonList = [];
 
-    {
-      name: 'Abra',
-      height: 8,
-      type: ['psychic']
-    },
-
-    {
-      name: 'Doduo',
-      height: 10,
-      type: ['normal, flying']
-    },
-
-    {
-      name: 'Psyduck',
-      height: 5,
-      type: ['water']
-    }
-  ];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
-    if (
-      typeof pokemon === "object" &&
-      "name" in pokemon &&
-      "height" in pokemon &&
-      "type" in pokemon
-    ) {
-      repository.push(pokemon);
-    } else {
-      console.log("pokemon is not correct");
-    }
+      pokemonList.push(pokemon);
   }
 
   function getAll() {
@@ -41,12 +15,12 @@ let pokemonRepository = (function () {
 
   function addListItem(pokemon){
 
-    //this section creates the buttons with details about the pokemon
+    //This section creates the buttons with details about the pokemon.
 
     let unorderedList = document.querySelector('.pokemon-ul');
     let listItem = document.createElement('li');
     let button = document.createElement('button');
-    button.innerText = pokemon.name + ' (type: ' + pokemon.type + ') (height: ' + pokemon.height + ') ';
+    button.innerText = pokemon.name;
     button.classList.add('pokemon-button');
     listItem.appendChild(button);
     unorderedList.appendChild(listItem);
@@ -58,24 +32,65 @@ let pokemonRepository = (function () {
     });
   }
 
-  //this function works within the addListItem function above to send pokemon details to the console when a button is clicked
+  //This section is the code for fetching Pokemon data from the Pokemon API.
 
-  function showDetails(pokemon){
-    console.log(pokemon)
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
   }
 
-  //this section makes these functions callable outside the IIFE.
+  //This section is the code for extracting Pokemon details from detailsUrl.
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  //This function sends pokemon details to the console.
+
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
+  }
+
+  //This section makes these functions callable outside the IIFE.
 
   return{
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails
   };
 })()
 
 
-//This section lists the Pokemons' names with their heights with a forEach loop.
+//This section creates a list of the Pokemons' names.
 
-pokemonRepository.getAll().forEach(
-  pokemonRepository.addListItem
-);
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
+});
