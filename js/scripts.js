1886fa5c5a85e44a40f932513e8ed1d35e8eda78
gbstrom <gbstrom@gmail.com>
2021-06-26 21:46:44 -0400

    added code to summon a modal with more details when a pokemon's name is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -70,9 +70,63 @@ let pokemonRepository = (function () {
 
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
-      console.log(pokemon);
+      //console.log(pokemon);
+
+      let modalContainer = document.querySelector('#modal-container');
+
+      // This section clears all existing modal content.
+      modalContainer.innerHTML = '';
+
+      let modal = document.createElement('div');
+      modal.classList.add('modal');
+
+      // Add the new modal content
+      let closeButtonElement = document.createElement('button');
+      closeButtonElement.classList.add('modal-close');
+      closeButtonElement.innerText = 'Close';
+      closeButtonElement.addEventListener('click', hideModal);
+
+      let titleElement = document.createElement('h1');
+      titleElement.innerText = pokemon.name;
+
+      let contentElement = document.createElement('p');
+      contentElement.innerText = 'height: ' + pokemon.height;
+
+      let imageElement = document.createElement('img');
+      imageElement.classList.add('modal-pokemon-image');
+      imageElement.src = pokemon.imageUrl;
+
+      modal.appendChild(closeButtonElement);
+      modal.appendChild(titleElement);
+      modal.appendChild(imageElement);
+      modal.appendChild(contentElement);
+      modalContainer.appendChild(modal);
+
+      modalContainer.classList.add('is-visible');
+
+      modalContainer.addEventListener('click', (e) => {
+        // Since this is also triggered when clicking INSIDE the modal
+        // We only want to close if the user clicks directly on the overlay
+        let target = e.target;
+        if (target === modalContainer) {
+        //I am confused here. Why isn't it '!==' here? We only want to close the modal if the user clicks somewhere outside the modal, not inside. this code does that, but I don't understand why. PUZZLE RESOLVED (partly) because the container covers the whole window area thanks to the CSS. I still don't understand why clicking inside the modal itself doesn't trigger this code, since after all clicking on the modal seems as if it would count as clicking on the modalContainer. But I will assume it's because I'm wrong about that, and that the modalContainer is not clickable if the modal is on top of it.
+          hideModal();
+        }
+      });
     });
+  };
+
+  function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
   }
+
+  window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('#modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
 
   //This section makes these functions callable outside the IIFE.
 
