78d8a5ef84b2691e8703878fe651c36aa294ea10
gbstrom <gbstrom@gmail.com>
2021-07-03 09:14:10 -0400

    replaced my modal with the bootstrap modal, adjusting css and js accordingly

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ let pokemonRepository = (function () {
 
   function addListItem(pokemon){
 
-    //This section creates the buttons with details about the pokemon.
+    //This section creates the buttons that lead to more details about the pokemon.
 
     let unorderedList = document.querySelector('.pokemon-ul');
     let listItem = document.createElement('li');
@@ -25,11 +25,13 @@ let pokemonRepository = (function () {
     button.classList.add('btn-primary');
     button.classList.add('btn-block');
     button.setAttribute("type", "button");
+    button.setAttribute("data-toggle", "modal");
+    button.dataset.target = "#pokemonModal";
     button.innerText = pokemon.name;
     listItem.appendChild(button);
     unorderedList.appendChild(listItem);
 
-    //this section makes clicking the buttons send pokemon details to the console
+    //this section makes clicking the buttons send pokemon details into a modal that pops up.
 
     button.addEventListener('click', function(){
       showDetails(pokemon);
@@ -70,66 +72,26 @@ let pokemonRepository = (function () {
     });
   }
 
-  //This function sends pokemon details to the console.
+  //This function sends pokemon details to the moczl.
 
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
-      //console.log(pokemon);
+      let modalContainer = document.querySelector('#pokemonModal');
 
-      let modalContainer = document.querySelector('#modal-container');
+      // This section puts info about the selected pokemon into the modal.
 
-      // This section clears all existing modal content.
-      modalContainer.innerHTML = '';
-
-      let modal = document.createElement('div');
-      modal.classList.add('modal');
-
-      // Add the new modal content
-      let closeButtonElement = document.createElement('button');
-      closeButtonElement.classList.add('modal-close');
-      closeButtonElement.innerText = 'Close';
-      closeButtonElement.addEventListener('click', hideModal);
-
-      let titleElement = document.createElement('h1');
-      titleElement.innerText = pokemon.name;
-
-      let contentElement = document.createElement('p');
-      contentElement.innerText = 'height: ' + pokemon.height;
+      let modalTitle = document.querySelector("#modalTitle");
+      modalTitle.innerText = pokemon.name;
+      let contentContainer = document.querySelector('.modal-body');
+      contentContainer.innerText = 'height: ' + pokemon.height;
 
       let imageElement = document.createElement('img');
       imageElement.classList.add('modal-pokemon-image');
       imageElement.src = pokemon.imageUrl;
 
-      modal.appendChild(closeButtonElement);
-      modal.appendChild(titleElement);
-      modal.appendChild(imageElement);
-      modal.appendChild(contentElement);
-      modalContainer.appendChild(modal);
-
-      modalContainer.classList.add('is-visible');
-
-      modalContainer.addEventListener('click', (e) => {
-        // Since this is also triggered when clicking INSIDE the modal
-        // We only want to close if the user clicks directly on the overlay
-        let target = e.target;
-        if (target === modalContainer) {
-          hideModal();
-        }
-      });
+      contentContainer.appendChild(imageElement);
     });
   };
-
-  function hideModal() {
-    let modalContainer = document.querySelector('#modal-container');
-    modalContainer.classList.remove('is-visible');
-  }
-
-  window.addEventListener('keydown', (e) => {
-    let modalContainer = document.querySelector('#modal-container');
-    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-      hideModal();
-    }
-  });
 
   //This section makes these functions callable outside the IIFE.
 
